17cab6091506c417d648f633cb6b71528eb85624
Sunghoon Ko <nsxt@outlook.com>
2015-06-08 20:42:00 +0900

    Test coding.

--- test/test.cpp
+++ test/test.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
+
+	std::for_each(
+		in(std::cin), in(), std::cout << (_1 * 3) << " ");
+	
 	return 0;
 }
 
